505f2814095bd65105af2a54a353ed3d24374679
Miftah <miftahaadem@gmail.com>
2024-10-07 22:25:03 +0300

    implement searchBar component and  home page

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,29 @@
-const SearchBar = () => {
-  return <div>SearchBar</div>
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSearch(searchTerm)
+    setSearchTerm('')
+  }
+  return (
+    <form onSubmit={handleSubmit} className="flex justify-center gap-3 ">
+      <input
+        type="text"
+        placeholder="Enter Movie Name"
+        value={searchTerm}
+        onChange={(event) => {
+          setSearchTerm(event.target.value)
+        }}
+        className="border rounded-2xl w-full max-w-xl shadow-md p-3 outline-none"
+      />
+      <button className="bg-blue-500  p-3 text-white  rounded-xl">
+        Search
+      </button>
+    </form>
+  )
 }
 
 export default SearchBar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,41 @@
+import { useState } from 'react'
+import SearchBar from '../components/SearchBar'
+import MovieCard from '../components/MovieCard'
 const Home = () => {
-  return <div>Home</div>
+  const [movies, setMovies] = useState([])
+  const [Loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const fetchMovieData = async (searchTerm) => {
+    try {
+      setLoading(true)
+      const response = await fetch(
+        `http://www.omdbapi.com/?s=${searchTerm}&apikey=615c1a03`
+      )
+      const data = await response.json()
+      setLoading(false)
+      setMovies(data.Search)
+    } catch (error) {
+      setError('Failed to fetch movies. Try again.')
+    }
+  }
+
+  if (Loading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+  console.log(movies)
+  return (
+    <div className="container mx-auto p-6">
+      {error && <p>{error}</p>}
+      <SearchBar onSearch={fetchMovieData} />
+      {movies &&
+        movies.map((movie) => <MovieCard movie={movie} key={movie.imdbID} />)}
+    </div>
+  )
 }
 
 export default Home
