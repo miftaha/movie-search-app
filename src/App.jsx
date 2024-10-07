7e023d1db66e69c13513caa856d209b80b8538f1
Miftah <miftahaadem@gmail.com>
2024-10-07 19:59:03 +0300

    create components,pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import MovieDetails from './components/MovieDetails'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
